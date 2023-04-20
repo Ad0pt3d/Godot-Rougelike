3629e698d296212669d7ec61a04d858d8da3b296
Ad0pt3d <rafimiah2005@gmail.com>
2023-04-20 09:35:25 -0400

    player scene and player script, test level, placeholder textures

--- player.gd
+++ player.gd
@@ -1,11 +1,18 @@
-extends Node
+extends CharacterBody2D
+
+@export var speed = 100
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func get_input():
+	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = input_dir * speed
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
